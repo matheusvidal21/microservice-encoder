883f44d6328419c08bfe11e18cc349f4b208f4a2
Matheus Vidal <matheusvidal140@gmail.com>
2024-06-09 18:53:19 -0300

    Implementando serviço de video para download e fragment

---
+++ application/services/video_service_test.go
@@ -0,0 +1,50 @@
+package services_test
+
+import (
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
+	"github.com/matheusvidal21/microservice-encoder/application/repositories"
+	"github.com/matheusvidal21/microservice-encoder/application/services"
+	"github.com/matheusvidal21/microservice-encoder/domain"
+	"github.com/matheusvidal21/microservice-encoder/framework/database"
+	"github.com/stretchr/testify/require"
+	"log"
+	"testing"
+	"time"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDatabaseTest()
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.New().String()
+	video.FilePath = "musica.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	return video, repo
+}
+
+func TestVideoService_Download(t *testing.T) {
+	video, repo := prepare()
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("video-storage-project")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+}
--- go.mod
+++ go.mod
@@ -1,19 +1,15 @@
 module github.com/matheusvidal21/microservice-encoder
 
-go 1.14
+go 1.13
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	cloud.google.com/go/storage v1.9.0
+	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535
 	github.com/google/uuid v1.6.0
-	github.com/jinzhu/gorm v1.9.16
-	github.com/lib/pq v1.10.9
-	github.com/stretchr/testify v1.8.1
-	gorm.io/driver/sqlite v1.1.3 // Versão compatível com Go 1.14
-	gorm.io/gorm v1.21.16 // Versão compatível com Go 1.14
-)
-
-require (
-	github.com/kr/pretty v0.3.0 // indirect
-	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	github.com/jinzhu/gorm v1.9.12
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.1.1
+	github.com/stretchr/testify v1.6.1
+	gorm.io/driver/sqlite v1.1.3
+	gorm.io/gorm v1.21.16
 )
